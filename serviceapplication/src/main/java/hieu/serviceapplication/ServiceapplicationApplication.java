980cbf2dc4fb86f575ab17d181ef891118d94a96
hieunm2 <hieunm2@vnext.vn>
2025-12-02 14:57:39 +0700

    add service application

---
+++ serviceapplication/src/main/java/hieu/serviceapplication/ServiceapplicationApplication.java
@@ -0,0 +1,15 @@
+package hieu.serviceapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServiceapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceapplicationApplication.class, args);
+    }
+
+}
